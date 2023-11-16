59ada4740bfde512acd87bbcc957f384d319a9a1
Pawat Sarnchawanakit <pawat.sar@ku.th>
2023-11-16 10:39:21 +0700

    Made run_ball.py OOp

--- run_ball.py
+++ run_ball.py
@@ -1,17 +1,23 @@
 import turtle
 import ball
 
-num_balls = int(input("Number of balls to simulate: "))
-turtle.speed(0)
-turtle.tracer(0)
-turtle.hideturtle()
-canvas_width = turtle.screensize()[0]
-canvas_height = turtle.screensize()[1]
-turtle.colormode(255)
-ballSystem = ball.BallSystem(ball.Vec2(canvas_width, canvas_height))
-ballSystem.addRandomBalls(num_balls);
-while (True):
-    turtle.clear()
-    ballSystem.tick();
-    turtle.update()
-turtle.done()
+class BallApp:
+    def __init__(self):
+        num_balls = int(input("Number of balls to simulate: "))
+        turtle.speed(0)
+        turtle.tracer(0)
+        turtle.hideturtle()
+        canvas_width = turtle.screensize()[0]
+        canvas_height = turtle.screensize()[1]
+        turtle.colormode(255)
+        self.__ballSystem = ball.BallSystem(ball.Vec2(canvas_width, canvas_height))
+        self.__ballSystem.addRandomBalls(num_balls);
+    def loop(self):
+        while (True):
+            turtle.clear()
+            self.__ballSystem.tick();
+            turtle.update()
+        turtle.done()
+
+
+BallApp().loop();
